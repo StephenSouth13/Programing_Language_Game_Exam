40ee5a68831961496bb28ede5e7a64311d1fe8f0
Quách Thành Long <stephensouth1307@gmail.com>
2025-06-23 11:35:31 +0700

    -update

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Firebase;
+using Firebase.Database;
+
+namespace ConsoleApp1
+{
+    
+    internal class Program
+    {
+        public class Player
+        {
+            public string PlayerID;
+            public string Name;
+            public int Gold;
+            public int Score;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
